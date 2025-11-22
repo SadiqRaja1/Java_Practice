86debd232cb290ff354e52fec08184fbad69c4f9
SadiqRaja1 <sortmode314@gmail.com>
2025-11-22 18:39:52 +0530

    trying to solve array one using hash

--- 6_Arrays/ArraysOne.java
+++ 6_Arrays/ArraysOne.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class ArraysOne {
 
     public static boolean dupCheck(int [] parameterInput) {
@@ -10,10 +12,21 @@ public class ArraysOne {
         }
         return false;
     }
+
+    public static void dupCheckHashsets (int parameterInput []) {
+        HashSet hash = new HashSet<Integer>();
+        hash.add(parameterInput[0]);
+        hash.addAll(parameterInput[1],parameterInput[2]);
+        System.out.println(hash.contains(1));
+
+        System.out.println(hash);
+    }
     public static void main(String[] args) {
         int input1 [] = {1,2,3,1};
         int input2 [] = {1,2,3,4};
         int input3 [] = {1,1,1,3,3,4,3,2,4,2};
+
+        dupCheckHashsets(input3);
 
         System.out.println("Input1 dupplicate present : "+ dupCheck(input1));
         System.out.println("Input2 dupplicate present : "+ dupCheck(input2));
