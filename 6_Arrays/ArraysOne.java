5ca6b4ba3d312de0e14b8dc2b3b8dadee999902d
SadiqRaja1 <sortmode314@gmail.com>
2025-11-23 16:35:41 +0530

    ArrayOne hashset Updated

--- 6_Arrays/ArraysOne.java
+++ 6_Arrays/ArraysOne.java
@@ -13,23 +13,31 @@ public class ArraysOne {
         return false;
     }
 
-    public static void dupCheckHashsets (int parameterInput []) {
-        HashSet hash = new HashSet<Integer>();
-        hash.add(parameterInput[0]);
-        hash.addAll(parameterInput[1],parameterInput[2]);
-        System.out.println(hash.contains(1));
-
-        System.out.println(hash);
+    public static boolean dupCheckHashsets (int parameterInput []) {
+        HashSet<Integer> hash = new HashSet<Integer>();
+        
+        for( int i = 0; i<parameterInput.length; i++) {
+            if(hash.contains(parameterInput[i])) {
+                return true;
+            }else {
+                hash.add(parameterInput[i]);
+            }
+        }
+        return false;
     }
     public static void main(String[] args) {
         int input1 [] = {1,2,3,1};
         int input2 [] = {1,2,3,4};
         int input3 [] = {1,1,1,3,3,4,3,2,4,2};
 
-        dupCheckHashsets(input3);
+        
 
         System.out.println("Input1 dupplicate present : "+ dupCheck(input1));
         System.out.println("Input2 dupplicate present : "+ dupCheck(input2));
         System.out.println("Input3 dupplicate present : "+ dupCheck(input3));
+
+        System.out.println("Input1 dupplicate present : "+ dupCheckHashsets(input1));
+        System.out.println("Input2 dupplicate present : "+ dupCheckHashsets(input2));
+        System.out.println("Input3 dupplicate present : "+ dupCheckHashsets(input3));
     }
 }
---
+++ 6_Arrays/ArraysTwo.java
@@ -0,0 +1,6 @@
+public class ArraysTwo {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
