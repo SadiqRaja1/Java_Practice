c12620f8faae5888d4afb1400f57819b80e4f32d
SadiqRaja1 <sortmode314@gmail.com>
2025-11-27 22:16:24 +0530

    butterFly pattern competed

--- 5.2_Pattern_part2/butterFly.java
+++ 5.2_Pattern_part2/butterFly.java
@@ -4,61 +4,35 @@ public class butterFly {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = 5;
         sc.close();
 
-        // for(int i=1; i<=n; i++){
-        //     for(int j=1; j<=i; j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
-        // for(int i=n; i>=1; i--) {
-        //     for(int j=1; j<=i; j++) {
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        int n = 5;
 
-        // int start = n-1;
-        // for(int i=1; i<=n; i++){
-        //     for(int j=1; j<=n; j++) {
-        //         if(j<=start) {
-        //             System.out.print(" ");
-        //         }else {
-        //             System.out.print("*");
-        //         }
-        //     }
-        //     start--;
-        //     System.out.println();
-        // }
-        int start = 1; int end = n*2;
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=n*2; j++) {
-                if(j>start && j<end) {
-                    System.out.print(" ");
-                }else {
-                    System.out.print("*");
-                }
+        for(int i=1; i<=5; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
             }
             System.out.println();
-            start++;
-            end--;
         }
 
-        start = 1;
-        end = n*2;
-        for(int i=1; i<=n; i++) {
-            for(int j=n*2; j>=1; j--) {
-                if(j>start && j<end) {
-                    System.out.print(" ");
-                }else {
-                    System.out.print("*");
-                }
+        for(int i=n; i>=1; i--) {
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
             }
             System.out.println();
-            start++;
-            end--;
+            
         }
     }
 }
