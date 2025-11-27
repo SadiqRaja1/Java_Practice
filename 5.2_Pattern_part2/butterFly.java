2a7eb84d86d0a04b7b061ce6617a01a6906a4a1d
SadiqRaja1 <sortmode314@gmail.com>
2025-11-27 22:30:23 +0530

    hollowRhombus completed

--- 5.2_Pattern_part2/butterFly.java
+++ 5.2_Pattern_part2/butterFly.java
@@ -4,11 +4,13 @@ public class butterFly {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+
+        System.out.println("Enter number");
+        int n = sc.nextInt();
         sc.close();
 
-        int n = 5;
-
-        for(int i=1; i<=5; i++) {
+        for(int i=1; i<=n; i++) {
             for(int j=1; j<=i; j++) {
                 System.out.print("*");
             }
