4bb6112c4c58ec9f13ea0e9f3a1d3edb1b94a3b7
SadiqRaja1 <sortmode314@gmail.com>
2025-12-25 19:21:57 +0530

    Assingment half done bubble and Selection sort completed

--- 7_Sorting/inbuild.java
+++ 7_Sorting/inbuild.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.Collections;
 
 public class inbuild {
     public static void main(String[] args) {
---
+++ 7_Sorting/sortingOne.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class sortingOne {
+
+    public static void bubbleSortReverse (int parameterArr []) {
+        for(int i = 0; i<parameterArr.length; i++) {
+            for(int j=0; j<parameterArr.length-1-i; j++) {
+                if(parameterArr[j] < parameterArr[j+1]) {
+                    int temp = parameterArr[j];
+                    parameterArr[j] = parameterArr[j+1];
+                    parameterArr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(parameterArr));
+    }
+
+    public static void SelectionSortReverse (int parameterArr []) {
+        for(int i = 0; i< parameterArr.length; i++) {
+            for(int j = i+1; j<parameterArr.length; j++) {
+                if(parameterArr[i] < parameterArr[j]) {
+                    int temp = parameterArr[i];
+                    parameterArr[i] = parameterArr[j];
+                    parameterArr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(parameterArr));
+    }
+    public static void main(String[] args) {
+        int arr [] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+
+        bubbleSortReverse(arr);
+        SelectionSortReverse(arr);
+    }
+}
