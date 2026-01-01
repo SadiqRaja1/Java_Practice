e39a69d7f89d1a1fa62f275b4cdeb62f0433d94b
SadiqRaja1 <sortmode314@gmail.com>
2026-01-01 06:47:19 +0530

    2d array basic 2 topic done

---
+++ 8_2DArrays/twoDArraysBasic.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class twoDArraysBasic {
+    
+    public static void main(String[] args) {
+
+        /*
+        // Creation of array 
+        Scanner sc = new Scanner(System.in);
+        int arr [] [] = new int [3][3];
+
+        for(int i=0; i<arr.length; i++) {
+            for(int j= 0; j<arr[i].length; j++) {
+                System.out.print("Enter number of intex [" + i + "],["+ j + "] -");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println(Arrays.deepToString(arr));
+
+        */
+
+        //Search in 2d Array
+
+        int arr [] [] = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int target = 7;
+
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr[0].length; j++) {
+                if(arr[i][j] == target) {
+                    System.out.println("Target located at index " + i+ " " + j);
+                }
+            }
+        }
+    }
+}
