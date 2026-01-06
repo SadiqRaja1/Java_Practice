e642219f563bc5659a2bfe2d0683cf36bfde9a12
SadiqRaja1 <sortmode314@gmail.com>
2026-01-06 07:03:10 +0530

    review montly 1st pattern part 2 hollowRhombus and diamond pattern completed

---
+++ Review/Monthly/1st_2nd_1_2026/5.2_Pattern_part2/diamond.java
@@ -0,0 +1,26 @@
+public class diamond {
+
+    public static void main(String[] args) {
+        int n = 4;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i * 2; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = 1; j < n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i * 2; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
