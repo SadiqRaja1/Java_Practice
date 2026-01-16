fc77f879f7512222c93d31f48f173fde8cc6e2d2
SadiqRaja1 <sortmode314@gmail.com>
2026-01-16 08:05:59 +0530

    Question 5 completed

---
+++ 9_Strings/StringFive.java
@@ -0,0 +1,14 @@
+public class StringFive {
+
+    public static void main(String[] args) {
+        //string intern used to check the pool for the same value or it will add it. as new String stored in heap. intern() over use not advisable
+        String a = new String("Sadiq");
+        String b = a.intern();
+
+        System.out.println(b);
+
+        //String is Slow, immutable, threadsafe / StringBuffer is Slower, mutable, threadsafe synchronized / StringBuilder Fast, muatble, non- threadSafe
+        StringBuffer c = new StringBuffer("Sadiq");
+        System.out.println(b);
+    }
+}
