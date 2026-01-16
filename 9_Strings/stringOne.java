c71ecd2fd83aebebbe786863cee0a2503d849ca7
SadiqRaja1 <sortmode314@gmail.com>
2026-01-16 07:00:00 +0530

    String question one and two completed

--- 9_Strings/stringOne.java
+++ 9_Strings/stringOne.java
@@ -1,10 +1,21 @@
 public class stringOne {
     public static void main(String[] args) {
         String a = "Shaikh Sadiq Raja Sardar";
-        String b = "A";
 
         System.out.println(count(a));
     }
 
-    
+    public static int count(String a) {
+        int count = 0;
+        for(int i=0; i<a.length(); i++) {
+            if( a.charAt(i) == 'a'||
+                a.charAt(i) == 'e'||
+                a.charAt(i) == 'i'||
+                a.charAt(i) == 'o'||
+                a.charAt(i) == 'u' ) {
+                    count++;
+            }
+        }
+        return count;
+    }
 }
---
+++ 9_Strings/stringTwo.java
@@ -0,0 +1,9 @@
+public class stringTwo {
+    public static void main(String[] args) {
+        String str = "ShradhaDidi";
+        String str2 = "ApnaCollege";
+        String str3 = "ShradhaDidi";
+
+        System.out.println(str.equals(str3) + " "+ str2.equals(str3));
+    }
+}
