3130e455721c3ac72a45401257bd8925757d34e5
SadiqRaja1 <sortmode314@gmail.com>
2026-02-05 23:08:29 +0530

    conditionalOne completed

---
+++ Review/Monthly/2nd_4th_2_2026/3_conditional/conditionalOne.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class conditionalOne {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number : ");
+
+        int a = sc.nextInt();
+
+        if(checkNum(a)) {
+            System.out.println("Number is negative");
+        }else{
+            System.out.println("Number is positive");
+        }
+    }
+
+    public static boolean checkNum (int a) {
+        return a < 0;
+    }
+}
