d23783567a747525ab2f887c4c39562d0abee4cd
SadiqRaja1 <sortmode314@gmail.com>
2026-02-07 11:41:35 +0530

    monthly review 2 loops completed

---
+++ Review/Monthly/2nd_4th_2_2026/4_Loops/loopsFive.java
@@ -0,0 +1,9 @@
+public class loopsFive {
+  public static void main (String args []) {
+   for(int i = 0; i <= 5; i++ ) {
+    System.out.println("i = " + i );
+   }
+   //System.out.prinln("i after loop" + i);
+   //i scole only inside loop
+  }
+}
