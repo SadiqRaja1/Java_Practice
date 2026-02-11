c430930bd7120376232b344861cffe1ef85fb116
SadiqRaja1 <sortmode314@gmail.com>
2026-02-11 22:18:51 +0530

    montly review 2 4.1 pattern part 1 completed

---
+++ Review/Monthly/2nd_4th_2_2026/4.1_Pattern_part1/character.java
@@ -0,0 +1,13 @@
+public class character {
+
+    public static void main(String[] args) {
+        char alphabet = 'A';
+
+        for(int i=0;i<5;i++) {
+            for(int j=0; j<=i; j++) {
+                System.out.print(alphabet++);
+            }
+            System.out.println();
+        }
+    }
+}
