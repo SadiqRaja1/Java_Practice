4e4e8dde6efd087fb56eb2734c16a0ee80e68950
SadiqRaja1 <sortmode314@gmail.com>
2026-02-15 06:03:58 +0530

    monthly review 2 butterfly and solidrhombus pattern completed

---
+++ Review/Monthly/2nd_4th_2_2026/5.2_Pattern_part2/triangle01.java
@@ -0,0 +1,17 @@
+public class triangle01 {
+
+    public static void main(String[] args) {
+        int n=5;
+
+        for(int i=0; i<5; i++){
+            for(int j=0; j<=i; j++){
+                if((i+j) % 2 == 0) {
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
